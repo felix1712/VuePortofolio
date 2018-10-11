b15e529d18f3c698012342d5b2cf4d9250b55895
lix <felix@talenta.co>
2018-10-11 16:08:00 +0700

    rebuild views and component

---
+++ src/components/BaseLandings/BaseLanding.vue
@@ -0,0 +1,5 @@
+<template src="./index.html"></template>
+
+export default{
+	name: 'BaseLanding',
+}
---
+++ src/views/AppHomes/AppHome.vue
@@ -0,0 +1,13 @@
+<template src="./index.html"></template>
+
+<script type="text/javascript">
+	import BaseLanding from '@/components/BaseLandings/BaseLanding.vue';
+
+	export default{
+		name: 'AppHome',
+		components: {
+			BaseLanding,
+		},
+	};
+
+</script>
