7a985bb08059ead6cfec299311ba1172e93bdcee
felix <felix@talenta.co>
2018-10-26 14:45:48 +0700

    unfixed base skill component and section

---
+++ src/components/BaseSkills/BaseSkill.vue
@@ -0,0 +1,15 @@
+<template src="./index.html"></template>
+
+<script>
+	import ContentHeading from '@/components/ContentHeadings/ContentHeading.vue';
+
+	export default{
+		name: 'BaseSkill',
+		components: {
+			ContentHeading,
+		},
+		props: {
+			dataSkills: Object,
+		},
+	};
+</script>
