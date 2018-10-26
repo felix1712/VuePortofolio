6c3082ada9e363932f225bc322fff7599080d71f
felix <felix@talenta.co>
2018-10-26 10:37:33 +0700

    add component base about

---
+++ src/components/BaseAbouts/BaseAbout.vue
@@ -0,0 +1,21 @@
+<template src="./index.html"></template>
+
+<script>
+	import ContentHeading from '@/components/ContentHeadings/ContentHeading.vue';
+
+	export default{
+		name: 'BaseAbout',
+		components: {
+			ContentHeading,
+		},
+		data(){
+			return{
+				aboutMeText: '',
+			}
+		},
+
+		created(){
+			this.aboutMeText = "Hello I’m Felix Andrean a Front-end developer, my first journey become a developer start at 2010, is simply dummy text 									of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since."
+		}
+	};
+</script>
