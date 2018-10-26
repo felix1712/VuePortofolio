3e92c3f6534cd6ffb84537de9ec42ea69729cd4c
felix <felix@talenta.co>
2018-10-26 13:06:43 +0700

    import data into app

--- src/views/AppHomes/AppHome.vue
+++ src/views/AppHomes/AppHome.vue
@@ -1,16 +1,22 @@
 <template src="./index.html"></template>
 
 <script type="text/javascript">
+	import data from '@/data/data.json';
 	import BaseLanding from '@/components/BaseLandings/BaseLanding.vue';
 	import BaseAbout from '@/components/BaseAbouts/BaseAbout.vue';
 	import BaseSkill from '@/components/BaseSkills/BaseSkill.vue';
 
-	export default{
+	export default {
 		name: 'AppHome',
 		components: {
 			BaseLanding,
 			BaseAbout,
 			BaseSkill,
+		},
+		data() {
+			return {
+				allData: data,
+			};
 		},
 	};
 
