7b506764e734365e836e71f6364b01754bfbfdfc
felix1712 <felix.andrean17@gmail.com>
2019-02-23 12:05:37 +0700

    remove base input

--- src/components/BaseContactForms/BaseContactForm.vue
+++ src/components/BaseContactForms/BaseContactForm.vue
@@ -1,12 +1,10 @@
 <template src="./index.html"></template>
 
 <script>
-	import BaseInput from '@/components/BaseInputs/BaseInput.vue';
 	
 	export default {
 		name: 'BaseContactForm',
 		components: {
-			BaseInput,
 		},
 	};
 </script>
