ec11cc0e7841469be4c944149e64e27e0a62ad8d
felix1712 <felix.andrean17@gmail.com>
2019-02-23 12:06:10 +0700

    fixing linting error

--- src/store/modules/landing.js
+++ src/store/modules/landing.js
@@ -1,11 +1,11 @@
-import data from '@/api/data'
+import data from '@/api/data';
 
-export default{
+export default {
 	namespaced: true,
-	state:{
+	state: {
 		items: {
 			name: '',
-			title:'',
+			title: '',
 			titleMean: '',
 			titleNoun: '',
 			buttonText: '',
@@ -13,27 +13,26 @@ export default{
 	},
 
 	getters: {
-
 	},
 
 	mutations: {
-		setLandingData(state, data){
-  		state.items.name = data.name;
-  		state.items.title = data.title;
-  		state.items.titleMean = data.title_mean;
-  		state.items.titleNoun = data.title_noun;
-  		state.items.buttonText = data.button_my_profile;
-  	},
+		setLandingData(state, datas) {
+			state.items.name = datas.name;
+			state.items.title = datas.title;
+			state.items.titleMean = datas.title_mean;
+			state.items.titleNoun = datas.title_noun;
+			state.items.buttonText = datas.button_my_profile;
+		},
 	},
 
 	actions: {
-		fetchData({commit}){
-  		return new Promise ((resolve, reject) => {
-  			data.getData(data => {
-					commit('setLandingData', data)
+		fetchData({ commit }) {
+			return new Promise((resolve) => {
+				data.getData((item) => {
+					commit('setLandingData', item);
 					resolve();
-				})
-  		})
-  	},
-	}
-}
+				});
+			});
+		},
+	},
+};
